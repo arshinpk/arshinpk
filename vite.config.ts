43a9827c515fe27d046722718a291bf9af33e3cb
ARSHIN P K <97457025+arshinpk@users.noreply.github.com>
2025-11-28 14:37:43 +0530

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/arshinpk/", // REQUIRED FOR GITHUB PAGES
+  base: "./", // REQUIRED FOR GITHUB PAGES
   server: {
     host: "::",
     port: 8080,
