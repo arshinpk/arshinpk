c0ab5e4e4d14268e4e8d0c79c5ba6af416859fa8
ARSHIN P K <97457025+arshinpk@users.noreply.github.com>
2025-11-28 14:32:53 +0530

    Set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/arshinpk/", // REQUIRED FOR GITHUB PAGES
   server: {
     host: "::",
     port: 8080,
